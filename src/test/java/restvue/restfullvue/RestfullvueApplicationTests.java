266cbac73e52cf4810dce90b756f088c5b477536
makovey <twomvlad@mail.ru>
2020-08-23 14:21:54 +0300

    Create Controller

---
+++ src/test/java/restvue/restfullvue/RestfullvueApplicationTests.java
@@ -0,0 +1,13 @@
+package restvue.restfullvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullvueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
